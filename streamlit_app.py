d8d97348b108b55617c2b68ac92cb931463a2a2a
MellyMel Melnyk <113502755+mataches520@users.noreply.github.com>
2022-09-24 04:54:10 +0300

    insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
+#adding jackfruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('thanks for adding ', add_my_fruit)
+
+#insert
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
