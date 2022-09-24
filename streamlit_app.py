1c286a9e443c23834364b4194a22478caafa0ad0
MellyMel Melnyk <113502755+mataches520@users.noreply.github.com>
2022-09-24 06:09:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,8 +53,8 @@ except URLError as e:
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #streamlit.dataframe(fruityvice_normalized) 
 ############################################################################
-stop here in order to avoid insert
-streamlit.stop()
+#stop here in order to avoid insert
+#streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("use warehouse compute_wh")
